edfcaa28352ea2739e9927295e41095f624f05e2
RicardoVas9991 <rvasquez9999@hotmail.com>
2024-09-21 18:47:33 -0400

    added solution

--- Module2/Problem3.java
+++ Module2/Problem3.java
@@ -21,14 +21,28 @@ public class Problem3 {
     // <T> turns this into a generic so it can take in any datatype, it'll be passed as an Object so casting is required
     static <T> void bePositive(T[] arr){
         System.out.println("Processing Array:" + Arrays.toString(arr));
-        //your code should set the indexes of this array
         Object[] output = new Object[arr.length];
-        //hint: use the arr variable; don't diretly use the a1-a4 variables
-        //TODO convert each value to positive
-        //set the result to the proper index of the output array and maintain the original data type
-        //hint: don't forget to handle the data types properly, the result datatype should be the same as the original datatype
         
-        //end edit section
+        // rev - 9/21/2024
+        for (int i = 0; i < arr.length; i++) {
+            T value = arr[i];
+
+            if (value instanceof Integer) {
+                output[i] = Math.abs((Integer) value);
+            } else if (value instanceof Double) {
+                output[i] = Math.abs((Double) value);
+            } else if (value instanceof String) {
+                String strVal = (String) value;
+                try {
+                    int intValue = Integer.parseInt(strVal);
+                    output[i] = String.valueOf(Math.abs(intValue));
+                } catch (NumberFormatException e) {
+                    output[i] = strVal;
+                }
+            } else {
+                output[i] = value;
+            }
+        }
 
         StringBuilder sb = new StringBuilder();
         for(Object i : output){
